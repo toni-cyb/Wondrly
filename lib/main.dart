f3c910133ce24ec380a12d445fd4b755fd0c2622
Toni Bobocel <kksen036@gmail.com>
2025-08-26 17:17:16 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,34 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print("Hello Luminiq!");
+  runApp(const LuminiqApp());
+}
+
+class LuminiqApp extends StatelessWidget {
+  const LuminiqApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Luminiq Ludogames',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Luminiq Ludogames")),
+      body: const Center(
+        child: Text("Bienvenue dans Luminiq 🎉"),
+      ),
+    );
+  }
 }
